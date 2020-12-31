aad0464bef50e8a5c058052e2089d0df9f6494bc
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-12-31 21:13:13 +0900

    create messages_controller.rb

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+    before_action :set_user, only: [ :new :create, :update, :destroy ]
+  
+    def new
+      @message = @user.messages.new
+    end
+  
+    def create
+      @message = @user.messages.create!(message_params)
+  
+      redirect_to @user
+    end
+
+    def update
+        @message = @user.messages.update!(message_params)
+    end
+  
+    private
+      def set_user
+        @user = current_user
+      end
+  
+      def message_params
+        params.require(:message).permit(:content)
+      end
+end
